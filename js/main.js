901228d71f1b25a0c1a531fbe8d181adcd1558dc
evtravnikova <elena@travnikova.pro>
2023-03-22 18:22:55 +0100

    8 kyu String repeat

--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@
 console.log('ddk4', '123')*/
 
 
+/*
 function joinStrings(string1, string2){
   return `${string1.toString()} ${string2.toString()}`;
 }
 
 console.log('ddk4', '123')
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(5, 'o'))
